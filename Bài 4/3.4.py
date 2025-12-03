bf3c8b080b9b0a503715b6aa24970a1f895c7578
trongkhoi1818-tech <trongkhoi1818@gmail.com>
2025-12-03 08:30:36 +0700

    Update 3.4.py

--- Bài 4/3.4.py
+++ Bài 4/3.4.py
@@ -1,5 +1,5 @@
 
-print("##################################")
+print("ho ten:hoang trong khoi:mssv 245752021610117")
 
 ds = input('danh sach:').split( )
 
@@ -9,3 +9,4 @@ print(ds)
 #in day vua nhap, moi phan tu tren mot dong
 for so in ds:
     print(so)
+
