87e4d8051d627357036021c9a418747b9276a38f
trongkhoi1818-tech <trongkhoi1818@gmail.com>
2025-12-03 08:24:19 +0700

    Update 3.13.py

--- Bài 4/3.13.py
+++ Bài 4/3.13.py
@@ -1,7 +1,8 @@
 
-print("##################################")
+print("ho ten:hoang trong khoi:mssv 245752021610117")
 #nhap vao list
 ds = input('nhap chuoi: ').split()
 
 #tim phan tu trong list
 print("vi tri cua chuoi abc la", ds.index('abc'))
+
