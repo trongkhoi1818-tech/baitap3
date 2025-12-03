548e8bb26f2e1edbc7caaaee3ef0a0a044bf5b3a
trongkhoi1818-tech <trongkhoi1818@gmail.com>
2025-12-03 08:17:49 +0700

    Update 2.2.py

--- Bài 3/2.2.py
+++ Bài 3/2.2.py
@@ -1,7 +1,8 @@
 
-print("########################")
+print("ho ten:hoang trong khoi:mssv 245752021610117")
 #chuong trinh voi ham sum
 def sum(a, b):
     return a+b
 c = sum(5, 8);
 print("Tong cua 5 va 8= " + str(c))
+
