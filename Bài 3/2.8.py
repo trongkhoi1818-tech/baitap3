b80de7cca01bcd6a082412576eace443b68806f0
trongkhoi1818-tech <trongkhoi1818@gmail.com>
2025-12-03 08:21:37 +0700

    Update 2.8.py

--- Bài 3/2.8.py
+++ Bài 3/2.8.py
@@ -1,5 +1,5 @@
 
-print("########################")
+print("ho ten:hoang trong khoi:mssv 245752021610117")
 
 import math
 pos = [0, 0]
@@ -22,3 +22,4 @@ while True:
         pass
 ####################
 print(int(round(math.sqrt(pos[1] ** 2 + pos[0] ** 2))))
+
