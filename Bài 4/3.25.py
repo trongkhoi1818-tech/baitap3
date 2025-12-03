faacb8a48bd87fc9d1c6f15643c016262033a422
trongkhoi1818-tech <trongkhoi1818@gmail.com>
2025-12-03 08:29:35 +0700

    Update 3.25.py

--- Bài 4/3.25.py
+++ Bài 4/3.25.py
@@ -1,5 +1,6 @@
 
-print("################################")
+print("ho ten:hoang trong khoi:mssv 245752021610117")
 numbers = input("Nhập danh sách số, cách nhau bởi dấu phẩy: ").split(',')
 odd_numbers = [num for num in numbers if int(num) % 2 != 0]
 print(','.join(odd_numbers))
+
