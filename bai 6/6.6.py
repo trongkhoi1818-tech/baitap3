39c909c57744e4cd6f15885250d7eaf0f9b80f49
trongkhoi1818-tech <trongkhoi1818@gmail.com>
2025-12-03 08:36:25 +0700

    Update 6.6.py

--- bai 6/6.6.py
+++ bai 6/6.6.py
@@ -1,5 +1,5 @@
 
-print("############################")
+print("ho ten:hoang trong khoi:mssv 245752021610117")
 class StringModifier:
     def __init__(self):
         self.input_string = ""
@@ -14,3 +14,4 @@ class StringModifier:
 modifier = StringModifier()
 modifier.get_String()
 modifier.print_String()
+
