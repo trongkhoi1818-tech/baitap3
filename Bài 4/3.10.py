6f04e0001667273367538bffc7a963f01b04aa61
trongkhoi1818-tech <trongkhoi1818@gmail.com>
2025-12-03 08:23:03 +0700

    Update 3.10.py

--- Bài 4/3.10.py
+++ Bài 4/3.10.py
@@ -1,7 +1,8 @@
 
-print("##################################")
+print("ho ten:hoang trong khoi:mssv 245752021610117")
 ds = input('nhap chuoi: ').split()
 x = ds[0:-1]
 for c in x:
     print(c)
     
+
