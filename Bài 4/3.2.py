64fc87854a9ae1bf186dd45c90ddf1cb437b5ba1
trongkhoi1818-tech <trongkhoi1818@gmail.com>
2025-12-03 08:27:38 +0700

    Update 3.2.py

--- Bài 4/3.2.py
+++ Bài 4/3.2.py
@@ -1,10 +1,11 @@
 
-print("##################################")
+print("ho ten:hoang trong khoi:mssv 245752021610117")
 
 s = input('Nhap chuoi:')
 for ch in s:
     if ch.strip(): #Bo qua dau space va tab
         print(ch.upper(), end='')
+
 
 
 
